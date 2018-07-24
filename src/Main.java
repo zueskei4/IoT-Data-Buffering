510f454bfa0c6abdccaec8bc18e3153d60a988bb
Unknown <1412817@hcmut.edu.vn>
2018-07-24 15:25:00 +0700

    add2

---
+++ src/Main.java
@@ -0,0 +1,52 @@
+import java.util.Date;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Sensor oxy_sensor= new OxygenSensor("oxy1");
+		Sensor temp_sensor= new TemperatureSensor("temp1");
+		Sensor pH_sensor= new PHSensor("pH1");
+		
+		SensorManager sensor_manager= new SensorManager();
+		sensor_manager.addSensor(oxy_sensor, new Location(1.1111, 2.2222));
+		sensor_manager.addSensor(temp_sensor, new Location(2.2222, 2.2222));
+		sensor_manager.addSensor(pH_sensor, new Location(3.3333, 2.2222));
+		
+		Exporter console_exporter= new ConsoleExporter("console1");
+		Exporter file_exporter= new FileExporter("file1");
+		Exporter ip_exporter= new IPExporter("ip1");
+		
+		ExporterManager exporter_manager= new ExporterManager();
+		exporter_manager.addExporter(console_exporter);
+		exporter_manager.addExporter(file_exporter);
+		exporter_manager.addExporter(ip_exporter);
+		
+		BufferManager buffer_manager= new BufferManager();
+		
+		BufferDataManager log= new BufferDataManager();
+
+		Location location1= new Location(0.0000, 1.5555);
+		Location location2= new Location(1.5555, 3.5555);
+		Date time= new Date();
+		while(true) {
+			log.loadData2Buffer();
+			
+			for(Exporter ex: exporter_manager.exporter_list) {
+				log.query(ex, location1, location2);
+				log.query(ex, time.getTime(), time.getTime()- 5, location1, location2);
+				log.query(ex, time.getTime(), time.getTime()- 5);
+			}
+			
+			sleep(5000);
+		}
+	}
+	
+	public static void sleep(int _time) {
+		try {
+			Thread.sleep(_time);
+		}
+		catch (Exception e) {};
+	}
+
+}
--- src/SensorManager.java
+++ src/SensorManager.java
@@ -1,20 +1,29 @@
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class SensorManager {
 	ArrayList<Sensor> sensor_list;
+	Map<Sensor, Location> sensor_map;
 	DataConverter converter;
 	
 	public SensorManager() {
 		sensor_list= new ArrayList<Sensor>();
+		sensor_map= new HashMap<Sensor, Location>();
 		converter= new DataConverter();
 	}
 	
-	public void addSensor (Sensor _sensor) {
+	public void addSensor (Sensor _sensor, Location _location) {
 		sensor_list.add(_sensor);
+		sensor_map.put(_sensor, _location);
 	}
 	
 	public int getSensorNumber() {
 		return sensor_list.size();
+	}
+	
+	public Location getLocation(Sensor _sensor) {
+		return sensor_map.get(_sensor);
 	}
 	
 	public Sensor getSensor(int _index) {
